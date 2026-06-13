d1ae3f586db46dba040d570236806a30f69dab97
emilianoagustin <pintos.emiliano@gmail.com>
2026-06-12 22:06:30 -0300

    Added name filter to products

--- src/main/java/com/techlab/productcrud/repository/ProductRepository.java
+++ src/main/java/com/techlab/productcrud/repository/ProductRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.domain.Pageable;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
   Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
+  Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
+  Page<Product> findByCategoryIdAndNameContainingIgnoreCase(Long categoryId, String name, Pageable pageable);
 }
