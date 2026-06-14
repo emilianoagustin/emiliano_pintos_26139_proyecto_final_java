d245b5e2e32dcba8ebe9147aa86414305778ff24
emilianoagustin <pintos.emiliano@gmail.com>
2026-06-13 23:30:13 -0300

    Finished filter by price range

--- src/main/java/com/techlab/productcrud/repository/ProductRepository.java
+++ src/main/java/com/techlab/productcrud/repository/ProductRepository.java
@@ -9,4 +9,5 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
   Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
   Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
   Page<Product> findByCategoryIdAndNameContainingIgnoreCase(Long categoryId, String name, Pageable pageable);
+  Page<Product> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
 }
